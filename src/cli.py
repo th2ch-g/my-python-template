776c316cec34a917d0dc85140cf4638ba1b4ace5
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2025-06-22 23:47:49 +0900

    Set up basic Python project structure and tooling.

---
+++ src/cli.py
@@ -0,0 +1,32 @@
+import argparse
+import sys
+
+from .logger import generate_logger
+
+LOGGER = generate_logger(__name__)
+
+
+def cli() -> None:
+    # make parser
+    parser = argparse.ArgumentParser(
+        description=(
+            "Input description here"
+        )
+    )
+
+    # subcommands
+    # subparsers = parser.add_subparsers()
+
+    # add options
+    # parser.add_argument("--option", type=str, default="default", help="Option description")
+
+    args = parser.parse_args()
+
+    # prohibit no args
+    # if len(sys.argv) == 1:
+    #     LOGGER.error(f"use {sys.argv[0]} --help")
+    #     sys.exit(1)
+
+    # log info
+    # LOGGER.info(f"{sys.argv[1]} called")
+    # LOGGER.info(f"{sys.argv[1]} finished")
